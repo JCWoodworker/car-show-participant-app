3d7c5c7415a1d10ecaed4136ab0357d81a43f83e
JCWoodworker <jc@rilocalwoodworks.com>
2022-09-16 22:30:48 -0400

    refactor votes router to account for user being logged in or not and only return logged in user's votes

--- server/src/routes/api/v1/votesRouter.js
+++ server/src/routes/api/v1/votesRouter.js
@@ -7,18 +7,22 @@ import { User, Vote } from "../../../models/index.js"
 const votesRouter = new express.Router()
 
 votesRouter.get('/', async (req, res) => {
-  try {
-    const user = req.user.id
-    const votes = await Vote.query()
+  if (typeof req.user === 'undefined') {
+    return res.status(401).json({ errors: 'Unauthorized.  Must be logged in to view/edit your votes' })
+  } else {
+    try {
+      const user = req.user.id
+      const votes = await Vote.query()
       .where('userId', '=', user)
-    const serializedVotes = await Promise.all(
-      votes.map(async (vote) => {
-        return await VoteSerializer.getSummary(vote)
-      })
-    )
-    return res.status(200).json({ votes: serializedVotes })
-  } catch (error) {
-    return res.status(500).json({ errors: error })
+      const serializedVotes = await Promise.all(
+        votes.map(async (vote) => {
+          return await VoteSerializer.getSummary(vote)
+        })
+        )
+        return res.status(200).json({ votes: serializedVotes })
+      } catch (error) {
+        return res.status(500).json({ errors: error })
+      }
   }
 })
 
