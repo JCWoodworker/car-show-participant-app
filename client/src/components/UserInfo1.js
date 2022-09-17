d0a2beca8d9aa808155961d0d21b1b7921baa0f4
JCWoodworker <jc@rilocalwoodworks.com>
2022-09-16 23:27:26 -0400

    updated user table and backend data.  setup front end fetch

--- client/src/components/UserInfo1.js
+++ client/src/components/UserInfo1.js
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { useContext } from "react"
 import UserInfo2 from "./UserInfo2"
+import { userContext } from "./App"
 
 const UserInfo1 = (props) => {
-  
+  const currentUser = useContext(userContext) ? useContext(userContext) : null
+  let message = null
+  currentUser ? message = 
+    <h3>Welcome back {currentUser.firstName}!</h3> 
+    : message = "Please sign in or register"
+
   return (
     <>
-      <h3>This is the first page of user info</h3>
+      {message}
       <UserInfo2 />
     </>
   )
