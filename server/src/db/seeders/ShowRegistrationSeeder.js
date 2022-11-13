c5ff02c619603592aa83a6169d3742a4b793cf84
JCWoodworker <jc@rilocalwoodworks.com>
2022-11-12 21:53:44 -0500

    updated seeder

--- server/src/db/seeders/ShowRegistrationSeeder.js
+++ server/src/db/seeders/ShowRegistrationSeeder.js
@@ -7,29 +7,25 @@ class ShowRegistrationSeeder {
         registrationNumber: 2,
         registeredCarId: 1,
         paymentType: "Online",
-        transactionId: "01faketransaction01",
-        transactionTimestamp: "2021-11-12T19:34:49.000Z"
+        transactionId: "01faketransaction01"
       },
       {
         registrationNumber: 4,
         registeredCarId: 2,
         paymentType: "Online",
-        transactionId: "02faketransaction02",
-        transactionTimestamp: "2021-11-12T19:34:49.000Z"
+        transactionId: "02faketransaction02"
       },
       {
         registrationNumber: 1,
         registeredCarId: 3,
         paymentType: "Online",
-        transactionId: "03faketransaction03",
-        transactionTimestamp: "2021-11-12T19:34:49.000Z"
+        transactionId: "03faketransaction03"
       },    
       {
         registrationNumber: 3,
         registeredCarId: 4,
         paymentType: "Cash",
-        transactionId: "04faketransaction04",
-        transactionTimestamp: "2021-11-12T19:34:49.000Z"
+        transactionId: "04faketransaction04"
       }  
     ]
 
